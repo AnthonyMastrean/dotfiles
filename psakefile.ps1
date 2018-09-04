34909eb588ff4c60ac4d063bb5da08a79a930f62
Anthony Mastrean <anthony.mastrean@gmail.com>
2018-09-04 10:27:32 -0400

    remove subversion

--- psakefile.ps1
+++ psakefile.ps1
@@ -40,20 +40,4 @@ Task Link {
 
         cmd /c mklink $link $target
     }
-
-    # -----[ subversion ]------------------------
-    $svndir = Join-Path $ENV:APPDATA 'Subversion'
-
-    New-Item -Force -Path $svndir -Type Directory | Out-Null
-
-    Get-ChildItem -File -Path 'src/.subversion' | %{
-        $link = Join-Path $svndir $_.Name
-        $target = $_.FullName
-
-        if (Test-Path $link) {
-            Remove-Item -Force -Path $link | Out-Null
-        }
-
-        cmd /c mklink $link $target
-    }
 }
